37b9cd326310b7b2f32459f8cf8e433f98c306df
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-09-15 17:23:33 +0900

    add C05

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,27 @@
+void	ft_putnbr(int nb)
+{
+	char	c;
+	if (nb < 0)
+	{
+		if (nb == -2147483648)
+		{
+			write(1, "-2147483648", 11);
+			return ;
+		}
+		else
+		{
+			write(1, "-", 1);
+			nb *= -1;
+		}
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		c = nb + '0';
+		write(1, &c, 1);
+	}
+}
---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,39 @@
+int	g_pm;
+
+int is_pm(char c)
+{
+	if (c == '-' || c == '+')
+	{
+		if (c == '-')
+			g_pm *= -1;
+		return (1);
+	}
+	else
+		return (0);
+}
+
+int	is_space(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+
+	i = 0;
+	g_pm = 1;
+	while (is_space(*str))
+		str++;
+	while (is_pm(*str))
+		str++;
+	while ('0' <= *str && *str <= '9')
+	{
+		i = i * 10 + (*str - '0');
+		str++;
+	}
+	return (i * g_pm);
+}
