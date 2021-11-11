25684640975c7a40535b509710db437bbb9f708b
jhon3242 <rldotnr2@gmail.com>
2021-11-11 23:12:17 +0900

    libft 함수  추가 대략 10 개 정도의 함수를 추가로 작성하였다. 헤더와 Wall Wextra Werror 는 검사했지만 추가로 놈 검사와 테스트케이스 검사가 필요하다.

--- borntocode/main.c
+++ borntocode/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include "ft.h"
 
 int main(void)
 {
-	printf("%d\n",ft_isalpha((int)("1")));
+	char test[] = "BlockDMask1234";
+	for (int i =0 ; i < (int)strlen(test); i++)
+	{
+		printf("%d ", ft_isalnum(test[i]));
+		printf("%d\n", isdigit(test[i]));
+
+
+	}
+
 	return (0);
 }
